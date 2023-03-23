b6884d2fa679b748380c8e74e85cb828357480e8
resakse <resakse@gmail.com>
2023-03-23 10:59:13 +0800

    Update CCT.py
    sort ikut tarikh, buang hrct lung

--- CCT.py
+++ CCT.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import PySimpleGUI as sg
 import pandas as pd
-import os
+from datetime import datetime
+harini = datetime.now()
+charini = datetime.strftime(harini,"%d-%m-%Y")
 
 def is_valid_path(filepath):
     if filepath and Path(filepath).exists():
@@ -15,7 +17,7 @@ def is_valid_path(filepath):
 
 
 ver = [
-    [sg.Text("Ver 1.1 2023, www.resakse.com", justification="right")],
+    [sg.Text("Ver 1.0 2023, www.resakse.com", justification="right")],
 ]
 layout = [
     [sg.Text("File Statistik"), sg.Push(), sg.Input(key="-IN-"),
@@ -29,10 +31,10 @@ layout = [
 def prosesfile(ct_input, ct_output):
     df = pd.read_excel(ct_input)
     filename = Path(ct_input).stem
-    outputfile = Path(ct_output) / f"convert_{filename}.xlsx"
+    outputfile = Path(ct_output) / f"{filename}-convert-{charini}.xlsx"
     # convert datetime
     df['DATE_OF_BIRTH'] = df['DATE_OF_BIRTH'].dt.strftime('%d/%m/%Y')
-   # df['ADDED_DATE'] = df['ADDED_DATE'].dt.strftime('%d/%m/%Y %H')
+    #df['ADDED_DATE'] = df['ADDED_DATE'].dt.strftime('%d/%m/%Y %H')
     df['NATIONAL_ID_NO'] = df['NATIONAL_ID_NO'].astype(str).str.replace('.0', '', regex=False)
     # buang column tak berkenaan
     del df['ORDER_TYPE_CODE']
@@ -40,14 +42,19 @@ def prosesfile(ct_input, ct_output):
     del df['DATE_OF_BIRTH']
     del df['STATUS']
     del df['RADIOLOGIST_ID']
-
-    df = df.sort_values(by=['PATIENT_ID', 'ADDED_DATE']) #sort kemudian shift & mark duplicate < 1 jam
+    ### Problem sebab 10.58am - 11.01am
+    unik = df[df['SHORT_DESC'] != 'CT Lungs (HRCT)']  # filter yang bukan hrct lung
+    # akhir = unik.drop_duplicates(
+    #     ['PATIENT_ID', 'ADDED_DATE'])  # buang duplicate rekod pt id yang sama dlm jam yang sama
+    # akhir.to_excel(outputfile, index=False)
+    df = unik.sort_values(by=['ADDED_DATE','PATIENT_ID']) #sort kemudian shift & mark duplicate < 1 jam
     mask = (df['PATIENT_ID'] == df['PATIENT_ID'].shift(1)) & (abs(df['ADDED_DATE'] - df['ADDED_DATE'].shift(1)) < pd.Timedelta('1 hour'))
     df.loc[mask, 'to_remove'] = True
     df = df[df.to_remove != True] # filter out yang kena mark
     del df['to_remove'] # buang column
     df.to_excel(outputfile, index=False)
     sg.popup("Siap! :)")
+
 
 window = sg.Window("Convert Statistik CT Scan", layout, icon=r'cticon.ico')
 
